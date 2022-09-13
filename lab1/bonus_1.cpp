1b33a5ab095c43948bc38f4b9a8e412dc9ac02b9
GraC2H5OH <Dont_Tapv2#g>
2022-09-13 20:14:54 +0300

    added bonus_1

--- lab1/bonus_1.cpp
+++ lab1/bonus_1.cpp
@@ -2,6 +2,34 @@
 
 using namespace std;
 
-int main(){
-    
+int pow(int x, int p) {
+    if (p == 1) {
+        return x;
+    }
+
+    if (p == 0) {
+        return 1;
+    }
+
+    return x * pow(x, p - 1);
+}
+
+int main() {
+    int num;
+    int one;
+    int zero;
+
+    cin >> num;
+
+    while (num > 0) {
+        if (num % 2 == 1) {
+            one += 1;
+        }
+        else {
+            zero += 1;
+        }
+        num /= 2;
+    }
+    cout << "Количество единиц >> " << one << "\n";
+    cout << "Количество нулей >> " << zero;
 }
