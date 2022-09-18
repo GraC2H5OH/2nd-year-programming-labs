23ec049f94dce953a1ec7996f092289d1971fafb
GraC2H5OH <starchenko879@gmail.com>
2022-09-18 21:57:55 +0300

    restart

--- lab2/3/headd.h
+++
@@ -1,3 +0,0 @@
-double Distance(double x_1, double y_1, double x_2, double y_2);
-double Square(double x_a, double y_a, double x_b, double y_b, double x_c, double y_c);
-double Distance_to_line(double x_a, double y_a, double x_1, double y_1, double x_2, double y_2);
